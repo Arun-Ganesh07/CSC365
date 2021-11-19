4a1d59b89a55a9bbac70103b835d157abf2a619a
Garrett-Ravatt <garrbear123456789@gmail.com>
2021-11-18 18:21:37 -0800

    Refactored Room class into its own file

--- Lab7/FR2.java
+++ Lab7/FR2.java
@@ -128,32 +128,4 @@ public class FR2 {
             nAdults = sc.nextInt();
     }
 
-    private class Room {
-        public String RoomCode;
-        public String RoomName;
-        public int beds;
-        public String bedType;
-        public int maxOcc;
-        public float basePrice;
-        public String decor;
-
-        Room(java.sql.ResultSet rs) throws SQLException{
-            RoomCode = rs.getString("RoomCode");
-            RoomName = rs.getString("RoomName");
-            beds = rs.getInt("beds");
-            bedType = rs.getString("bedType");
-            basePrice = rs.getFloat("basePrice");
-            maxOcc = rs.getInt("maxOcc");
-            decor = rs.getString("decor");
-        }
-
-        public String toString() {
-            return String.join(" ",
-                    RoomCode, RoomName, String.valueOf(beds),
-                    bedType, String.valueOf(basePrice),
-                    String.valueOf(basePrice), String.valueOf(maxOcc),
-                    decor
-            );
-        }
-    }
 }
